68d2ff90630f5376be4318f952290b0c5ea4fcf2
AlexLeMes <alexlem17@gmail.com>
2017-12-14 12:46:05 +0400

    re-added lookat script

---
+++ Extech/Assets/scripts/lookat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookat : MonoBehaviour {
+
+    public GameObject objectToLookAt;
+    Vector3 target;
+
+    private void Start()
+    {
+        target = objectToLookAt.transform.position;
+    }
+
+
+    void Update ()
+    {
+        target = objectToLookAt.transform.position;
+
+        transform.LookAt(target);
+    }
+}
